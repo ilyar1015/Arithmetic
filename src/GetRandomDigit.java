4cffe96301065d6a19cafa51129db44593c673b2
ilyar1015 <ilyar1015@hotmail.com>
2018-03-20 16:06:10 +0800

    201803201606

--- src/GetRandomDigit.java
+++ src/GetRandomDigit.java
@@ -2,12 +2,12 @@
 public class GetRandomDigit {
 	//0~4随机数
 	int oprator(){
-		return (int)(Math.random()*4);
+		return (int)(Math.random()*5+1-0.0001);
 	}
 	
 	//3~5的随机数
 	int Time(){
-		return (int)(Math.random()*2+3);
+		return (int)(Math.random()*2+3.5);
 	}
 	
 	//0~100 随机数
--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,9 @@ public class Main {
 	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		String path = "result.txt";
+		if(args.length==0){
+			System.out.println("qwe");
+		}
 //		int n=Integer.parseInt(args[0]);
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
